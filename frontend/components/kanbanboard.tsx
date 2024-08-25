0cbbe817231229cc45ecd1023971c731345afe22
RoecDesenvolvimentoWeb <roec.roecdesenvolvimentoweb@gmail.com>
2024-08-25 18:28:14 -0300

    feat(kanbanboard): adiciona funcionalidade de criar listas

--- frontend/components/kanbanboard.tsx
+++ frontend/components/kanbanboard.tsx
@@ -1,31 +1,47 @@
-"use client";
+import { useState } from "react";
 import { useKanban } from "@/contexts/KanbanContext";
-import { Board, Pipelines } from "@/types/kanban";
 import KanbanColumn from "./kanbancolumn";
-import { useEffect } from "react";
+import { Button } from "./ui/button";
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogFooter, DialogTitle } from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Pipelines } from "@/types/kanban";
 
-const KanbanBoard = () => {
-  const { selectedPipeline, pipelines } = useKanban();
+const KanbanBoard = ({ className }: any) => {
+  const { selectedPipeline, pipelines, addBoard } = useKanban();
+  const [newBoardName, setNewBoardName] = useState("");
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const pipelineKey = selectedPipeline as keyof Pipelines;
 
-  useEffect(() => {
-    console.log("Selected Pipeline:", selectedPipeline);
-    console.log("Boards:", pipelines[pipelineKey]);
-  }, [selectedPipeline, pipelines, pipelineKey]);
-
-  console.log("Renderizando KanbanBoard com pipeline:", selectedPipeline);
-  console.log("Boards para esse pipeline:", pipelines[pipelineKey]);
-
-  useEffect(() => {
-    console.log("Pipeline mudou no KanbanBoard:", selectedPipeline);
-  }, [selectedPipeline]);
+  const handleAddBoard = () => {
+    if (newBoardName.trim() !== "") {
+      addBoard(pipelineKey, newBoardName);
+      setNewBoardName("");
+      setIsDialogOpen(false);
+    }
+  };
 
   return (
     <div className="flex space-x-4">
-      {pipelines[pipelineKey]?.map((board: Board) => (
+      {pipelines[pipelineKey]?.map((board: any) => (
         <KanbanColumn key={board.id} board={board} pipelineName={selectedPipeline} />
       ))}
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogTrigger asChild>
+          <Button className="w-full max-w-[200px] bg-[#87A644]">Adicionar etapa</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Adicionar Nova Lista</DialogTitle>
+          </DialogHeader>
+          <Input value={newBoardName} onChange={(e) => setNewBoardName(e.target.value)} placeholder="Nome da nova lista" />
+          <DialogFooter>
+            <Button className="bg-[#87A644] w-full" onClick={handleAddBoard}>
+              Salvar
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
